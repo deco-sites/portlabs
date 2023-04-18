2682bb83ce43bebfc8d2bad4b4cb9a62d56a9d27
Luan Fonseca <luanfonceca@gmail.com>
2023-04-18 14:52:14 -0300

    Rethinking the legacyCss plugin.

--- utils/legacyCssPlugin.ts
+++ utils/legacyCssPlugin.ts
@@ -3,24 +3,13 @@ import { Plugin } from "$fresh/server.ts";
 const STYLE_ELEMENT_ID = "__FRSH_LEGACY_CSS";
 const LEGACY_CSS_PATH = "./static/css/legacy.min.css";
 
-export default function legacyCss(options?: { selfURL: string }): Plugin {
-  const main = `data:application/javascript,import hydrate from "${
-    new URL("./utils/legacyCss.ts", import.meta.url).href
-  }";
-  import options from "${options?.selfURL}";
-  export default function(state) { hydrate(options, state); }`;
+export default function legacyCss(): Plugin {
   return {
-    name: "twind",
-    entrypoints: { "main": main },
+    name: "legacyCss",
     render(ctx) {
-      const res = ctx.render();
+      ctx.render();
       const cssText = Deno.readTextFileSync(LEGACY_CSS_PATH);
-      const scripts = [];
-      if (res.requiresHydration) {
-        scripts.push({ entrypoint: "main", state: [] });
-      }
       return {
-        scripts,
         styles: [{ cssText, id: STYLE_ELEMENT_ID }],
       };
     },
