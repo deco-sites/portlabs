e01ba9510cce0fcb85ce337d68a7090e136e349b
Luan Fonseca <luanfonceca@gmail.com>
2023-04-21 10:58:41 -0300

    Trying to fix fbevents. 3/n

--- sections/Head.tsx
+++ sections/Head.tsx
@@ -310,7 +310,7 @@ export default function HeadComponent({
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                'https://pages.labcodes.com.br/proxy?url=https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer','${gtmKey}');
               `),
           }}
@@ -326,11 +326,11 @@ export default function HeadComponent({
               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+              })(window,document,'script','https://pages.labcodes.com.br/proxy?url=https://www.google-analytics.com/analytics.js','ga');
     
               ga('create', '${googleAnalyticsKey}', 'auto');
               ga('send', 'pageview');
-            `
+            `,
           }}
         >
         </script>
@@ -338,47 +338,58 @@ export default function HeadComponent({
 
       {linkedinInsightTagKey && (
         <>
-          <script defer type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
+          <script
+            defer
+            type="text/partytown"
+            dangerouslySetInnerHTML={{
+              __html: `
               _linkedin_partner_id = "${linkedinInsightTagKey}";
               window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
               window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-            `
-          }}>
+            `,
+            }}
+          >
           </script>
-          <script defer type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
+          <script
+            defer
+            type="text/partytown"
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(){var s = document.getElementsByTagName("script")[0];
               var b = document.createElement("script");
               b.type = "text/javascript";b.async = true;
-              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              b.src = "https://pages.labcodes.com.br/proxy?url=https://snap.licdn.com/li.lms-analytics/insight.min.js";
               s.parentNode.insertBefore(b, s);})();
-            `
-          }}>
+            `,
+            }}
+          >
           </script>
-          <noscript dangerouslySetInnerHTML={{
-            __html: `
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
               <img
                 height="1"
                 width="1"
                 style="display:none;"
                 alt=""
-                src='https://px.ads.linkedin.com/collect/?pid=${linkedinInsightTagKey}&fmt=gif'
+                src='https://pages.labcodes.com.br/proxy?url=https://px.ads.linkedin.com/collect/?pid=${linkedinInsightTagKey}&fmt=gif'
               />
-            `
-          }}>
+            `,
+            }}
+          >
           </noscript>
         </>
       )}
 
       {leadfeederTrackerKey && (
-        <script type="text/partytown" dangerouslySetInnerHTML={{
-          __html: `
-            (function () { window.ldfdr = window.ldfdr || {}; (function (d, s, ss, fs) { fs = d.getElementsByTagName(s)[0]; function ce(src) { var cs = d.createElement(s); cs.src = src; setTimeout(function () { fs.parentNode.insertBefore(cs, fs) }, 1); } ce(ss); })(document, 'script', 'https://sc.lfeeder.com/lftracker_v1_${leadfeederTrackerKey}.js'); })();
-          `
-        }}>
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function () { window.ldfdr = window.ldfdr || {}; (function (d, s, ss, fs) { fs = d.getElementsByTagName(s)[0]; function ce(src) { var cs = d.createElement(s); cs.src = src; setTimeout(function () { fs.parentNode.insertBefore(cs, fs) }, 1); } ce(ss); })(document, 'script', 'https://pages.labcodes.com.br/proxy?url=https://sc.lfeeder.com/lftracker_v1_${leadfeederTrackerKey}.js'); })();
+          `,
+          }}
+        >
         </script>
       )}
 
