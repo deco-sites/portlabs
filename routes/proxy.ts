6935101a771b3b8aab1aac709dcbcae6493acc5e
Luan Fonseca <luanfonceca@gmail.com>
2023-04-21 11:08:12 -0300

    Trying to fix fbevents. 5/n

--- routes/proxy.ts
+++ routes/proxy.ts
@@ -5,8 +5,7 @@ const ALLOWLIST_URLS = [
   "https://connect.facebook.net/en_US/fbevents.js",
   "https://www.facebook.com/tr?id=196893035003333&ev=PageView&noscript=1",
   "https://connect.facebook.net/signals/config/196893035003333?v=2.9.102&r=stable",
-  "https://www.googletagmanager.com/gtm.js",
-  "https://www.google-analytics.com/analytics.js",
+  "https://www.googletagmanager.com/gtm.js?id=GTM-PG7VBQ3",
   "https://snap.licdn.com/li.lms-analytics/insight.min.js",
   "https://px.ads.linkedin.com/collect/?pid=1609042&fmt=gif",
 ];
@@ -16,6 +15,7 @@ export const handler: Handlers = {
     const url = (new URL(req.url)).searchParams.get("url");
 
     if (!url || !ALLOWLIST_URLS.includes(url)) {
+      console.log('url: ', url)
       return new Response(null, { status: 404 });
     }
 
