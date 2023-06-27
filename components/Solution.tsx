db65da3c518d2488823ab2f6ff02e0fcff128cc6
Juliana Barros Lima <judiembl@gmail.com>
2023-06-27 20:14:06 -0300

    fix: adjusting image size for desktop screens

--- components/Solution.tsx
+++ components/Solution.tsx
@@ -17,22 +17,19 @@ export interface Props {
 
 export default function ({
   backgroundColor = "coral-40",
-  image =
-    "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
-  desktopImage =
-    "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
+  image = "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
+  desktopImage = "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
   title = "Solution",
   titleColor = "mineral-80",
   subtitle = "A FASTER, SLICKER, AND MORE FLEXIBLE SOFTWARE",
   subtitleColor = "mineral-80",
-  text =
-    "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
+  text = "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
 }) {
   return (
     <section class={`solution-splendid-spoon bg-${backgroundColor}`}>
-      <div class="solution-splendid-spoon__row pt-0">
+      <div class="solution-splendid-spoon__row items-stretch pt-0">
         <div class="solution-splendid-spoon__image xl:max-w-[40%]">
-          <Picture class="solution-splendid-spoon__first-img 2lg:hidden mb-0 bg-none h-[280px] md:h-[520px] lg:h-[480px]">
+          <Picture class="solution-splendid-spoon__first-img 2lg:hidden pt-12 object-scale-down">
             <Source
               media="(max-width: 767px)"
               src={image}
@@ -53,27 +50,25 @@ export default function ({
               src={image}
             />
           </Picture>
-          <Picture class="solution-splendid-spoon__first-img hidden 2lg:block mb-0 bg-none h-[900px]">
+          <Picture class="solution-splendid-spoon__first-img hidden 2lg:block mb-0 self-auto">
             <Source
               media="(min-width: 1200px)"
               src={desktopImage}
               fetchPriority="low"
               width={559}
-              height={900}
+              height={500}
             />
             <img
-              class="object-cover object-center-top h-full w-full md:rounded-br-[160px]"
+              class=" w-full object-cover md:rounded-br-[160px]"
               loading="lazy"
               src={desktopImage}
             />
           </Picture>
         </div>
-        <div class="solution-splendid-spoon__content pt-12">
-          <div class="solution-splendid-spoon__text solution-splendid-spoon__text--solution">
+        <div class="solution-splendid-spoon__content">
+          <div class="solution-splendid-spoon__text pt-16 pb-14 solution-splendid-spoon__text--solution">
             <h2 class={`text-${titleColor}`}>{title}</h2>
-            <p class={`topic-heading text-${subtitleColor}`}>
-              {subtitle}
-            </p>
+            <p class={`topic-heading text-${subtitleColor}`}>{subtitle}</p>
 
             <div dangerouslySetInnerHTML={{ __html: text }} />
           </div>
