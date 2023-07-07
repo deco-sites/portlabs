50fa83b9505636574260c89c1a07a69c1dd863e7
Juliana Barros Lima <judiembl@gmail.com>
2023-07-07 18:29:52 -0300

    feat: updating deps and minor fixes at the Solution img positioning

--- components/Solution2.tsx
+++ components/Solution2.tsx
@@ -13,17 +13,13 @@ export interface Props {
 
 export default function ({
   backgroundColor = "coral-40",
-  image =
-    "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
+  image = "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
   title = "subtitle",
   titleColor = "mineral-80",
-  text =
-    "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
+  text = "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
 }) {
   return (
-    <section
-      class={`solution-splendid-spoon bg-${backgroundColor}`}
-    >
+    <section class={`solution-splendid-spoon lg:pt-96 bg-${backgroundColor}`}>
       <div class="solution-splendid-spoon__row pb-0">
         <div class="solution-splendid-spoon__content">
           <div class="solution-splendid-spoon__text solution-splendid-spoon__text--marketing-team">
@@ -35,8 +31,7 @@ export default function ({
         <div class="solution-splendid-spoon__image">
           <span
             class="solution-splendid-spoon__second-img"
-            style={{ backgroundImage: `url(${image})` }}
-          >
+            style={{ backgroundImage: `url(${image})` }}>
             &nbsp;
           </span>
         </div>
--- live.gen.ts
+++ live.gen.ts
@@ -32,6 +32,7 @@ import * as $$$$$$$$16 from "./sections/Challenge.tsx";
 import * as $$$$$$$$17 from "./sections/HighlightsWithNumbers.tsx";
 import * as $$$$$$$$18 from "./sections/HeroWithImage.tsx";
 import * as $$$$$$$$19 from "./sections/Photo.tsx";
+import * as $live_workflows from "$live/routes/live/workflows/run.ts";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -72,11 +73,10 @@ import * as i2$$$$$$$$9 from "$live/matchers/MatchUserAgent.ts";
 import * as i2$$$$$$$$$0 from "$live/flags/audience.ts";
 import * as i2$$$$$$$$$1 from "$live/flags/everyone.ts";
 import * as i2$$$$$$$$$2 from "$live/flags/flag.ts";
-import * as i1$$$$$$3 from "$live/flags/multivariate.ts";
+import * as i2$$$$$$$$$3 from "$live/flags/multivariate.ts";
 import * as i2$$$$$$0 from "$live/actions/workflows/cancel.ts";
-import * as i2$$$$$$1 from "$live/actions/workflows/run.ts";
-import * as i2$$$$$$2 from "$live/actions/workflows/signal.ts";
-import * as i2$$$$$$3 from "$live/actions/workflows/start.ts";
+import * as i2$$$$$$1 from "$live/actions/workflows/signal.ts";
+import * as i2$$$$$$2 from "$live/actions/workflows/start.ts";
 import * as i2$0 from "deco-sites/std/functions/butterCMSAds.ts";
 import * as i2$1 from "deco-sites/std/functions/butterCMSBrands.ts";
 import * as i2$2 from "deco-sites/std/functions/butterCMSCategories.ts";
@@ -139,22 +139,22 @@ import * as i2$$$$$12 from "deco-sites/std/sections/SEO.tsx";
 import * as i2$$$$$13 from "deco-sites/std/sections/SEOPDP.tsx";
 import * as i2$$$$$14 from "deco-sites/std/sections/SEOPLP.tsx";
 import * as i2$$$$$15 from "deco-sites/std/sections/VTEXPortalDataLayerCompatibility.tsx";
-import * as i2$$$$$$4 from "deco-sites/std/actions/vtex/cart/addItems.ts";
-import * as i2$$$$$$5 from "deco-sites/std/actions/vtex/cart/getInstallment.ts";
-import * as i2$$$$$$6 from "deco-sites/std/actions/vtex/cart/removeItemAttachment.ts";
-import * as i2$$$$$$7 from "deco-sites/std/actions/vtex/cart/removeItems.ts";
-import * as i2$$$$$$8 from "deco-sites/std/actions/vtex/cart/simulation.ts";
-import * as i2$$$$$$9 from "deco-sites/std/actions/vtex/cart/updateAttachment.ts";
-import * as i2$$$$$$10 from "deco-sites/std/actions/vtex/cart/updateCoupons.ts";
-import * as i2$$$$$$11 from "deco-sites/std/actions/vtex/cart/updateItemAttachment.ts";
-import * as i2$$$$$$12 from "deco-sites/std/actions/vtex/cart/updateItemPrice.ts";
-import * as i2$$$$$$13 from "deco-sites/std/actions/vtex/cart/updateItems.ts";
-import * as i2$$$$$$14 from "deco-sites/std/actions/vtex/cart/updateProfile.ts";
-import * as i2$$$$$$15 from "deco-sites/std/actions/vtex/cart/updateUser.ts";
-import * as i2$$$$$$16 from "deco-sites/std/actions/vtex/newsletter/subscribe.ts";
-import * as i2$$$$$$17 from "deco-sites/std/actions/vtex/notifyme.ts";
-import * as i2$$$$$$18 from "deco-sites/std/actions/vtex/wishlist/addItem.ts";
-import * as i2$$$$$$19 from "deco-sites/std/actions/vtex/wishlist/removeItem.ts";
+import * as i2$$$$$$3 from "deco-sites/std/actions/vtex/cart/addItems.ts";
+import * as i2$$$$$$4 from "deco-sites/std/actions/vtex/cart/getInstallment.ts";
+import * as i2$$$$$$5 from "deco-sites/std/actions/vtex/cart/removeItemAttachment.ts";
+import * as i2$$$$$$6 from "deco-sites/std/actions/vtex/cart/removeItems.ts";
+import * as i2$$$$$$7 from "deco-sites/std/actions/vtex/cart/simulation.ts";
+import * as i2$$$$$$8 from "deco-sites/std/actions/vtex/cart/updateAttachment.ts";
+import * as i2$$$$$$9 from "deco-sites/std/actions/vtex/cart/updateCoupons.ts";
+import * as i2$$$$$$10 from "deco-sites/std/actions/vtex/cart/updateItemAttachment.ts";
+import * as i2$$$$$$11 from "deco-sites/std/actions/vtex/cart/updateItemPrice.ts";
+import * as i2$$$$$$12 from "deco-sites/std/actions/vtex/cart/updateItems.ts";
+import * as i2$$$$$$13 from "deco-sites/std/actions/vtex/cart/updateProfile.ts";
+import * as i2$$$$$$14 from "deco-sites/std/actions/vtex/cart/updateUser.ts";
+import * as i2$$$$$$15 from "deco-sites/std/actions/vtex/newsletter/subscribe.ts";
+import * as i2$$$$$$16 from "deco-sites/std/actions/vtex/notifyme.ts";
+import * as i2$$$$$$17 from "deco-sites/std/actions/vtex/wishlist/addItem.ts";
+import * as i2$$$$$$18 from "deco-sites/std/actions/vtex/wishlist/removeItem.ts";
 
 const manifest = {
   "functions": {
@@ -202,6 +202,7 @@ const manifest = {
     "./routes/live/previews/index.tsx": $live_previews_index,
     "./routes/live/release.ts": $live_release,
     "./routes/live/workbench.ts": $live_workbench,
+    "./routes/live/workflows/run.ts": $live_workflows,
     "./routes/proxy.ts": $$$$2,
   },
   "islands": {
@@ -302,29 +303,28 @@ const manifest = {
     "$live/flags/audience.ts": i2$$$$$$$$$0,
     "$live/flags/everyone.ts": i2$$$$$$$$$1,
     "$live/flags/flag.ts": i2$$$$$$$$$2,
-    "$live/flags/multivariate.ts": i1$$$$$$3,
+    "$live/flags/multivariate.ts": i2$$$$$$$$$3,
   },
   "actions": {
     "$live/actions/workflows/cancel.ts": i2$$$$$$0,
-    "$live/actions/workflows/run.ts": i2$$$$$$1,
-    "$live/actions/workflows/signal.ts": i2$$$$$$2,
-    "$live/actions/workflows/start.ts": i2$$$$$$3,
-    "deco-sites/std/actions/vtex/cart/addItems.ts": i2$$$$$$4,
-    "deco-sites/std/actions/vtex/cart/getInstallment.ts": i2$$$$$$5,
-    "deco-sites/std/actions/vtex/cart/removeItemAttachment.ts": i2$$$$$$6,
-    "deco-sites/std/actions/vtex/cart/removeItems.ts": i2$$$$$$7,
-    "deco-sites/std/actions/vtex/cart/simulation.ts": i2$$$$$$8,
-    "deco-sites/std/actions/vtex/cart/updateAttachment.ts": i2$$$$$$9,
-    "deco-sites/std/actions/vtex/cart/updateCoupons.ts": i2$$$$$$10,
-    "deco-sites/std/actions/vtex/cart/updateItemAttachment.ts": i2$$$$$$11,
-    "deco-sites/std/actions/vtex/cart/updateItemPrice.ts": i2$$$$$$12,
-    "deco-sites/std/actions/vtex/cart/updateItems.ts": i2$$$$$$13,
-    "deco-sites/std/actions/vtex/cart/updateProfile.ts": i2$$$$$$14,
-    "deco-sites/std/actions/vtex/cart/updateUser.ts": i2$$$$$$15,
-    "deco-sites/std/actions/vtex/newsletter/subscribe.ts": i2$$$$$$16,
-    "deco-sites/std/actions/vtex/notifyme.ts": i2$$$$$$17,
-    "deco-sites/std/actions/vtex/wishlist/addItem.ts": i2$$$$$$18,
-    "deco-sites/std/actions/vtex/wishlist/removeItem.ts": i2$$$$$$19,
+    "$live/actions/workflows/signal.ts": i2$$$$$$1,
+    "$live/actions/workflows/start.ts": i2$$$$$$2,
+    "deco-sites/std/actions/vtex/cart/addItems.ts": i2$$$$$$3,
+    "deco-sites/std/actions/vtex/cart/getInstallment.ts": i2$$$$$$4,
+    "deco-sites/std/actions/vtex/cart/removeItemAttachment.ts": i2$$$$$$5,
+    "deco-sites/std/actions/vtex/cart/removeItems.ts": i2$$$$$$6,
+    "deco-sites/std/actions/vtex/cart/simulation.ts": i2$$$$$$7,
+    "deco-sites/std/actions/vtex/cart/updateAttachment.ts": i2$$$$$$8,
+    "deco-sites/std/actions/vtex/cart/updateCoupons.ts": i2$$$$$$9,
+    "deco-sites/std/actions/vtex/cart/updateItemAttachment.ts": i2$$$$$$10,
+    "deco-sites/std/actions/vtex/cart/updateItemPrice.ts": i2$$$$$$11,
+    "deco-sites/std/actions/vtex/cart/updateItems.ts": i2$$$$$$12,
+    "deco-sites/std/actions/vtex/cart/updateProfile.ts": i2$$$$$$13,
+    "deco-sites/std/actions/vtex/cart/updateUser.ts": i2$$$$$$14,
+    "deco-sites/std/actions/vtex/newsletter/subscribe.ts": i2$$$$$$15,
+    "deco-sites/std/actions/vtex/notifyme.ts": i2$$$$$$16,
+    "deco-sites/std/actions/vtex/wishlist/addItem.ts": i2$$$$$$17,
+    "deco-sites/std/actions/vtex/wishlist/removeItem.ts": i2$$$$$$18,
   },
   "accounts": {
     "deco-sites/std/accounts/nuvemShop.ts": i2$$0,
