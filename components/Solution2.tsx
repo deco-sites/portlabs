f547e8bdb4dc8c3c033002af98a195aff8867c35
Juliana Barros Lima <judiembl@gmail.com>
2023-07-07 18:32:05 -0300

    fix: minor lint fixes

--- components/Solution2.tsx
+++ components/Solution2.tsx
@@ -13,10 +13,12 @@ export interface Props {
 
 export default function ({
   backgroundColor = "coral-40",
-  image = "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
+  image =
+    "https://labcodes.com.br/static/bundles/assets/src/imgs/cases/splendid/photo-solution.png",
   title = "subtitle",
   titleColor = "mineral-80",
-  text = "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
+  text =
+    "<p>Explicação do que é o projeto e quem é o cliente. E se essas poucas mulheres existem é porque, segundo alguns criativos homens, é importante ter uma visão feminina. Óbvio, quem iria pensar em campanhas de Dia das Mães ou de empoderamento feminino? Criativa só serve para pensar nisso mesmo. Ah, e para outras coisas também: como deixar a criação “mais bonita”, ouvir piadinha sobre feminismo e ser subestimada.</p>",
 }) {
   return (
     <section class={`solution-splendid-spoon lg:pt-96 bg-${backgroundColor}`}>
@@ -31,7 +33,8 @@ export default function ({
         <div class="solution-splendid-spoon__image">
           <span
             class="solution-splendid-spoon__second-img"
-            style={{ backgroundImage: `url(${image})` }}>
+            style={{ backgroundImage: `url(${image})` }}
+          >
             &nbsp;
           </span>
         </div>
